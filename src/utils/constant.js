ef679944dfeb15c1a9a8ac003984ed2ff2bf3a79
Nilesh Khairnar <nkhairnar543@gmail.com>
2024-12-04 23:35:12 +0530

    changed

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,12 +1,8 @@
 export const USER_API_END_POINT =
-  "http://localhost:3000/api/v1/user" ||
   "https://job-portal-website-backend-3o8x.onrender.com/api/v1/user";
 export const JOB_API_END_POINT =
-  "http://localhost:3000/api/v1/job" ||
   "https://job-portal-website-backend-3o8x.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:3000/api/v1/application" ||
   "https://job-portal-website-backend-3o8x.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "http://localhost:3000/api/v1/company" ||
   "https://job-portal-website-backend-3o8x.onrender.com/api/v1/company";
